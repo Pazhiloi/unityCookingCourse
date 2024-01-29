2c04ce9357ac2a6b106f1cbbbcf8382e18450398
Miroslav Miroslavov <martinsen06@gmail.com>
2024-01-29 15:20:29 +0200

    8. Collision Detection

--- KitchenChaos/Assets/Scripts/Player.cs
+++ KitchenChaos/Assets/Scripts/Player.cs
@@ -4,22 +4,54 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [SerializeField] private float moveSpeed = 7f;
-    [SerializeField] private GameInput gameInput;
+  [SerializeField] private float moveSpeed = 7f;
+  [SerializeField] private GameInput gameInput;
 
-    private bool isWalking;
-    private void Update() {
-      Vector2 inputVector = gameInput.GetMovementVectorNormalized();
+  private bool isWalking;
+  private void Update()
+  {
+    Vector2 inputVector = gameInput.GetMovementVectorNormalized();
 
-      Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
-      transform.position += moveDir * moveSpeed * Time.deltaTime;
+    Vector3 moveDir = new Vector3(inputVector.x, 0, inputVector.y);
 
-      isWalking = moveDir != Vector3.zero;
-      float rotateSpeed = 10f;
-      transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+
+    float moveDistance = moveSpeed * Time.deltaTime;
+    float playerRadius = 0.7f;
+    float playerHeight = 2f;
+    bool canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDir, moveDistance);
+
+    if (!canMove)
+    {
+      Vector3 moveDirX = new Vector3(moveDir.x, 0, 0).normalized;
+      canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirX, moveDistance);
+
+      if (canMove)
+      {
+        moveDir = moveDirX;
+      }else{
+        Vector3 moveDirZ = new Vector3(0, 0, moveDir.z).normalized;
+        canMove = !Physics.CapsuleCast(transform.position, transform.position + Vector3.up * playerHeight, playerRadius, moveDirZ, moveDistance);
+
+        if (canMove)
+        {
+          moveDir = moveDirZ;
+        }else{
+
+        }
+      }
     }
 
-    public bool IsWalking => isWalking;
+    if (canMove)
+    {
+      transform.position += moveDir * moveDistance;
+    }
+
+    isWalking = moveDir != Vector3.zero;
+    float rotateSpeed = 10f;
+    transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
+  }
+
+  public bool IsWalking => isWalking;
 
 
 }
